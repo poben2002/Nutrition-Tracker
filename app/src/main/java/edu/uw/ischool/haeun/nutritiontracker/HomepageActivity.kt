bd8382690233e32b876d74a9b3c62c95d879902d
poben2002 <benjaminpo2002@gmail.com>
2024-03-06 15:22:57 -0800

    added homepage

--- app/src/main/java/edu/uw/ischool/haeun/nutritiontracker/HomepageActivity.kt
+++ app/src/main/java/edu/uw/ischool/haeun/nutritiontracker/HomepageActivity.kt
@@ -2,10 +2,30 @@ package edu.uw.ischool.haeun.nutritiontracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
 
 class HomepageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.homepage)
+
+        val nutritionDashBoardButton: Button = findViewById(R.id.nutritionDashboardButton);
+        val notificationButton: Button = findViewById(R.id.notificationButton)
+        val searchBarButton: Button = findViewById(R.id.searchBarButton)
+        val userSettingsButton: Button = findViewById(R.id.userSettingsButton)
+
+        nutritionDashBoardButton.setOnClickListener {
+            startActivity(Intent(this, AddDataActivity::class.java))
+        }
+        notificationButton.setOnClickListener {
+            startActivity(Intent(this, NotificationSettingActivity::class.java))
+        }
+        searchBarButton.setOnClickListener {
+            startActivity(Intent(this, SearchDataActivity::class.java))
+        }
+        userSettingsButton.setOnClickListener {
+            startActivity(Intent(this, UserSettingActivity::class.java))
+        }
     }
 }
